bb4bf8132a6c6ac7e61a0cd9e123def1cfc7eed5
Adrian Miasik <AdrianMiasik@gmail.com>
2019-05-05 23:33:14 -0400

    Add scene name to example scene

---
+++ Assets/Scripts/ReplaceTextWithSceneName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[ExecuteInEditMode]
+public class ReplaceTextWithSceneName : MonoBehaviour
+{
+	[SerializeField] protected TMP_Text text;
+
+	private void Reset()
+	{
+		text = GetComponent<TMP_Text>();
+	}
+
+	private void Update()
+	{
+		text.text = SceneManager.GetActiveScene().name;
+	}
+}
